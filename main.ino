3bed46dff07b6c8e6a2413df9bc3a435b8acc8ad
Sameh Mohamed <sam@webiken.net>
2016-09-19 22:49:26 -0400

    updating the arduino main file

--- main.ino
+++ main.ino
@@ -1,26 +1,26 @@
-#include <iostream>
 #include <vector>
 
-#include "UltrasonicSensor.h"
+#include "UltraSonicSensor.h"
 #include "LaserSensor.h"
 
 using namespace std;
 
-
-
 long distance_, begin_, wait_;
 
 UltrasonicSensors sensor1(26);
+// planning for the future
 LaserSensor sensor2();
 
+vector<Sensor*> sensors;
+sensors.push_back(&sensor1);
+sensors.push_back(&sensor2);
 
 DcMotor motors[] = {DcMotor(7, 30, 31), DcMotor(8, 32, 33)};
-Driver driver(motors, sensor);
+Driver driver(motors);
 
 // reactive agent where all
 // the work is done
-ReactiveAgent agent(driver);
-
+ReactiveAgent agent(sensors, driver);
 
 // wait to start
 begin_ = millis();
